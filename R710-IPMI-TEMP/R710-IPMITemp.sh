31cd1780a0d71f5f550de1b64f08454d8d6ee482
timidtogekiss <michael@mikansystems.com>
2021-03-24 13:13:57 -0700

    add revert fan speed to manual when temp in range

--- R710-IPMI-TEMP/R710-IPMITemp.sh
+++ R710-IPMI-TEMP/R710-IPMITemp.sh
@@ -6,7 +6,6 @@
 #
 # Requires:
 # ipmitool – apt-get install ipmitool
-# slacktee.sh – https://github.com/course-hero/slacktee
 # ----------------------------------------------------------------------------------
 
 
@@ -21,21 +20,21 @@ IPMIEK=0000000000000000000000000000000000000000
 # TEMPERATURE
 # Change this to the temperature in celcius you are comfortable with.
 # If the temperature goes above the set degrees it will send raw IPMI command to enable dynamic fan control
-MAXTEMP=27
+MAXTEMP=32
+DESIREDTEMP=27
 
 # This variable sends a IPMI command to get the temperature, and outputs it as two digits.
 # Do not edit unless you know what you do.
 TEMP=$(ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK sdr type temperature |grep Ambient |grep degrees |grep -Po '\d{2}' | tail -1)
 
 
-if [[ $TEMP > $MAXTEMP ]];
-  then
-    printf "Warning: Temperature is too high! Activating dynamic fan control! ($TEMP C)" | systemd-cat -t R710-IPMI-TEMP
-    echo "Warning: Temperature is too high! Activating dynamic fan control! ($TEMP C)" | /usr/bin/slacktee.sh -t "R710-IPMI-TEMP [$(hostname)]"
+if [[ $TEMP > $MAXTEMP ]]; then
+    echo "Temp is above max threshold. Fans reverting to automatic."
     ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x01
+  elif [[ $TEMP > $DESIREDTEMP ]]; then
+    echo "Temp is between max and desired. Fans Unchanged."
   else
-    # healthchecks.io
-    curl -fsS --retry 3 https://hchk.io/XXX >/dev/null 2>&1
-    printf "Temperature is OK ($TEMP C)" | systemd-cat -t R710-IPMI-TEMP
-    echo "Temperature is OK ($TEMP C)"
+    echo "Temperature returned to below desired threshold. Fans dropping speed."
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x01 0x00
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW -y $IPMIEK raw 0x30 0x30 0x02 0xff 0x09
 fi
