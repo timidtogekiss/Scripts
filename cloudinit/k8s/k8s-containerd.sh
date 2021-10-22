d7d0c027ab81ebc8243a5698747ebdb6527066b3
Michael Rowland <michael@mikansystems.com>
2021-10-22 15:14:57 -0700

    add cloudinit k8s script for containerd setup

---
+++ cloudinit/k8s/k8s-containerd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+modprobe overlay
+modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sysctl --system
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -y kubectl kubelet kubeadm containerd.io
+apt-mark hold kubectl kubelet kubeadm containerd.io
+mkdir -p /etc/containerd
+wget https://raw.githubusercontent.com/timidtogekiss/cloudinit-test/master/containerd.toml
+mv containerd.toml /etc/containerd/config.toml
+systemctl restart containerd
